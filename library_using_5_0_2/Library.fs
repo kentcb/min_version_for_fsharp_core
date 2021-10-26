3bf560b9484859cef41427190e38eecf364cae31
Kent Boogaart <kent.boogaart@gmail.com>
2021-10-26 14:59:33 +1000

    Use interpolated string to prove FSharp.Core 5+

--- library_using_5_0_2/Library.fs
+++ library_using_5_0_2/Library.fs
@@ -1,5 +1,8 @@
 ﻿namespace library_using_5_0_2
 
 module Say =
+    // This will only work with FSharp.Core 5+
+    let interpolated = $"hello {42}"
+
     let hello name =
         printfn "Hello %s" name
